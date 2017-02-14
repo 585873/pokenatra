a968e61498f68216ebf4772ea4b5015737a3c618
Gurpreet Gill <gill_gurpreet2@bah.com>
2017-02-14 15:05:36 -0500

    Initial commit

---
+++ app.rb
@@ -0,0 +1,24 @@
+require "sinatra"
+require "sinatra/reloader"
+require 'active_record'
+
+# Load the file to connect to the DB
+require_relative 'db/connection.rb'
+
+# Load specific routes / controllers
+require_relative 'models/pokemon'
+
+#This is the index page
+get "/" do
+  @pokemons = Pokemon.all
+     erb :"pokemons/index"
+end
+
+get '/pokemons/new' do
+     erb :"pokemons/new"
+end
+
+get '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemons/show"
+end
